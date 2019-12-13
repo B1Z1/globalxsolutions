edddd9624a19586ac619054fb3f60bdf5396cdf3
ilyamishkin <iluhamishkin@gmail.com>
2019-12-13 23:31:04 +0100

    Created contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { styleVariables } from '../shared/styles/variables'
 
 import SEO from '../components/seo'
 import ContactForm from '../components/ContactForm'
@@ -23,7 +24,15 @@ class ContactPage extends React.Component {
         <p>Poniedziałek – Piątek pomiędzy 09:00 – 17:00</p>
         <p>
           Skontaktuj się z nami: +48 789 421 478 <br /> lub napisz
-          office@globalxsolutions.pl
+          <a
+            style={{
+              borderBottom: `1px solid ${styleVariables.colors.operate}`,
+            }}
+            href="mailto: office@globalxsolutions.pl"
+          >
+            {' '}
+            office@globalxsolutions.pl
+          </a>
         </p>
         <p style={{ paddingBottom: '32px' }}>
           © 2018 globalxsolutions.pl All rights reserved.
