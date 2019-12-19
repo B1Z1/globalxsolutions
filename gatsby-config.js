0c0b1b3cd3a119ca9baef93a6911b6114ed2b823
ilyamishkin <iluhamishkin@gmail.com>
2019-12-19 19:14:40 +0100

    Changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,7 +81,6 @@ module.exports = {
     ],
   },
   plugins: [
-    `@contentful/gatsby-transformer-contentful-richtext`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-typescript`,
