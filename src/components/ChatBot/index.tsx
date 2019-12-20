98d8ee1344ff31bb9e4dc90527dc89d18c11d96a
ilyamishkin <iluhamishkin@gmail.com>
2019-12-20 15:54:39 +0100

    Changes

--- src/components/ChatBot/index.tsx
+++ src/components/ChatBot/index.tsx
@@ -138,13 +138,7 @@ export default props => (
               description
               name
               profession
-              featuredImage {
-                childImageSharp {
-                  fluid(maxWidth: 200) {
-                    ...GatsbyImageSharpFluid
-                  }
-                }
-              }
+              featuredImage
             }
           }
         }
